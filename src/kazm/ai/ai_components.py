98d0e385c9510c37fc0127c399c2c8c320c40536
hamad <60110135+HAlafeefi@users.noreply.github.com>
2025-05-29 16:14:01 +0400

    Some basic things... Removing boilerplate

--- src/kazm/ai/ai_components.py
+++ src/kazm/ai/ai_components.py
@@ -1,24 +1,41 @@
 import os
 from openai import OpenAI
-from kazm.game.sus import A
+from kazm import settings
+
 
 # TODO add a history stuff for session speaking and overall with npc and stuff
+ai_model_name = settings.get("AI", "ai_model_name")
+
+api_key = settings.get("AI", "API_KEY")
+if settings.get("AI", "API_KEY_TYPE") == "env":
+    api_key = os.environ.get(api_key)
 
 
-client = OpenAI(
-    base_url="https://api.groq.com/openai/v1",
-    api_key=os.environ.get("GROQ_API_KEY")
-)
+custom_ai_op = settings.get("AI", "custom_ai")
+if custom_ai_op == "false":
+    client = OpenAI(api_key=api_key)
+else:
+    client = OpenAI(
+    base_url=custom_ai_op,
+    api_key=api_key)
 
-completion = client.chat.completions.create(
-    model="llama-3.1-8b-instant",
+
+def simple_ai_chat(msg: str, system_msg: str|None = None) -> str: 
     messages=[
-    {"role": "system", "content": "Talk like a pirate."},
     {
         "role": "user",
-        "content": "hey, how are you doing?",
-    },
-],
-)
+        "content": msg,
+    }]
 
-print(completion.choices[0].message.content)
+    if system_msg:
+        messages.insert(0, {"role": "system", "content": system_msg})
+    
+    completion = client.chat.completions.create(
+    model=ai_model_name, messages=messages)
+
+    return completion.choices[0].message.content
+
+
+if __name__ == '__main__':
+    print(simple_ai_chat(system_msg="How are you too smart?", msg="piracy?"))
+    
