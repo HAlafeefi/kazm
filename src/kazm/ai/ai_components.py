a042975c15c5ebf4661accb2dfa7a8a9496de2f5
hamad <60110135+HAlafeefi@users.noreply.github.com>
2025-05-29 04:18:12 +0400

    Initializing...

---
+++ src/kazm/ai/ai_components.py
@@ -0,0 +1,23 @@
+import os
+from openai import OpenAI
+from kazm.game.sus import A
+
+# TODO add a history stuff for session speaking and overall with npc and stuff
+
+client = OpenAI(
+    base_url="https://api.groq.com/openai/v1",
+    api_key=os.environ.get("GROQ_API_KEY")
+)
+
+completion = client.chat.completions.create(
+    model="llama-3.1-8b-instant",
+    messages=[
+    {"role": "system", "content": "Talk like a pirate."},
+    {
+        "role": "user",
+        "content": "hey, how are you doing?",
+    },
+],
+)
+
+print(completion.choices[0].message.content)
